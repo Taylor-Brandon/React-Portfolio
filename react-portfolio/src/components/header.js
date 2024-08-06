c1b178f827b7da6a9e5cfa23fb313943dbbe2537
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-06 17:41:01 -0400

    added nav links and header to nav

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <div></div>
+        <div>
+            <nav>
+            <h1>Developer name</h1>
+            <ul>
+                <li>about me</li>
+                <li>portfolio</li>
+                <li>contact</li>
+                <li>resume</li>
+            </ul>
+            </nav>
+        </div>
     );
 }
