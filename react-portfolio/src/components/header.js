ecca58f1859162bcc733811f7de0be5b1ed50d1c
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-07 12:33:54 -0400

    added color to links and render based on which one is active

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Info from './info';
+import Portfolio from './portfolio';
+import Contact from './contact';
+import Resume from './resume';
 
-export default function Header() {
-    return (
-        <div>
-            <nav>
-            <h1>Developer name</h1>
-            <ul>
-                <li>about me</li>
-                <li>portfolio</li>
-                <li>contact</li>
-                <li>resume</li>
-            </ul>
-            </nav>
-        </div>
-    );
-}
+const Header = () => {
+    const [currentLink, setCurrentLink] = useState(<Info />);
+
+    const handleClick = (link) => {
+        setCurrentLink(link);
+      };
+
+    const renderComponent = () => {
+        switch (currentLink) {
+            case 'info':
+            return <Info />
+            case 'portfolio':
+            return <Portfolio />
+            case 'contact':
+            return <Contact />
+            case 'resume':
+            return <Resume />
+         }
+    };
+    const getLinkStyle = (link) => ({
+        color: currentLink === link ? 'red' : 'black',
+      });
+  return (
+<div>
+      <nav>
+        <h1>Developer Name</h1>
+        <ul>
+          <li style={getLinkStyle('info')}>
+            <button onClick={() => handleClick('info')}>About Me</button>
+          </li>
+          <li style={getLinkStyle('portfolio')}>
+            <button onClick={() => handleClick('portfolio')}>Portfolio</button>
+          </li>
+          <li style={getLinkStyle('contact')}>
+            <button onClick={() => handleClick('contact')}>Contact</button>
+          </li>
+          <li style={getLinkStyle('resume')}>
+            <button onClick={() => handleClick('resume')}>Resume</button>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        {renderComponent()}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
