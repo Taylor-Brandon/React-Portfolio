f981c31e91c121207e6ea8428eae9c44a176871b
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-07 15:57:42 -0400

    added links to projects'

--- react-portfolio/src/components/portfolio.js
+++ react-portfolio/src/components/portfolio.js
@@ -4,6 +4,14 @@ export default function Portfolio() {
     return (
         <div>
             <h1>Portfolio</h1>
+            <ul>
+                <li><a href='https://github.com/Taylor-Brandon/ReadmeFile-Generator'></a></li>
+                <li><a href='https://github.com/Taylor-Brandon/Notes-App'></a></li>
+                <li><a href='https://github.com/Taylor-Brandon/E-Commerce'></a></li>
+                <li><a href='https://github.com/Taylor-Brandon/Employee-Database'></a></li>
+                <li><a href='https://github.com/Taylor-Brandon/Css-Wireframe'></a></li>
+                <li><a href='https://github.com/Taylor-Brandon/Blog-Website'></a></li>
+            </ul>
         </div>
     );
 }
