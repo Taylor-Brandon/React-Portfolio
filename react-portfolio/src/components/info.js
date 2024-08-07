5058c1bb249bbf4e1e3dd3e80f72f87698c00dbe
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-07 13:07:26 -0400

    added img and info paragraph to info component" "

--- react-portfolio/src/components/info.js
+++ react-portfolio/src/components/info.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import img from '../images/img.png';
+
+
 
 export default function Info() {
     return (
         <div>
             <h1>Info</h1>
+            <p>this is my paragraph</p>
+            <img src={img} alt='image of me' />
         </div>
     );
 }
