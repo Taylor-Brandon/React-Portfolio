9496592cd077d1ae5b24911e615faf6b49825e70
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-08 11:05:51 -0400

    added contact form to contact component

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -4,6 +4,17 @@ export default function Contact() {
     return (
         <div>
             <h1>Contact</h1>
+            <form>
+                <div className='mb-3'>
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email-input" aria-describedby="emailHelp"></input>
+                </div>
+                <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password-input"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 }
