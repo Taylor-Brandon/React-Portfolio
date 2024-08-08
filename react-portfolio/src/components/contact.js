4f546e1dda4f42b3c014d0a2af41b1c96d919007
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-08-08 12:07:58 -0400

    added form alerts and functionality

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (name === "name") {
+            setName(value);
+        } else if (name === "email") {
+            setEmail(value);
+        } else if (name === "message") {
+            setMessage(value);
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        const emailRegularexpression = /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/;
+
+        if(!emailRegularexpression.test(email)) {
+            alert('Please set email to valid email address!');
+            return;
+        }
+
+        alert(`Thank you so much, ${name}!`);
+        setName('');
+        setEmail('');
+        setMessage('');
+    };
+
+    const handleCursor = () => {
+        alert('Please enter your name!');
+    };
+
     return (
         <div>
             <h1>Contact</h1>
-            <form>
-            <div className='mb-3'>
-                <label for="name" class="form-label">Name</label>
-                <input type="name" class="form-control" id="name-input" aria-describedby="name-Help"></input>
+            <form className='form'>
+                <div className='mb-3'>
+                    <label className="form-label">Name</label>
+                    <input 
+                        value={name} 
+                        name="name" 
+                        onChange={handleInputChange} 
+                        onBlur={handleCursor}
+                        type="text" 
+                        placeholder='Name'
+                    />
                 </div>
                 <div className='mb-3'>
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email-input" aria-describedby="emailHelp"></input>
+                    <label className="form-label">Email address</label>
+                    <input 
+                        value={email} 
+                        name="email" 
+                        onChange={handleInputChange} 
+                        type="text" 
+                        placeholder='Email'
+                    />
                 </div>
-                <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password-input"></input>
+                <div className="mb-3">
+                    <label className="form-label">Message</label>
+                    <input 
+                        value={message} 
+                        name="message" 
+                        onChange={handleInputChange} 
+                        type="text" 
+                        placeholder='Message'
+                    />
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" onClick={handleFormSubmit} className="btn btn-primary">Submit</button>
             </form>
         </div>
     );
