e4fcef4a6f4ae7c451bbb7780d873343fdccd728
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-12-12 14:40:15 -0500

    added contact form, api to add functionality, and styled with animation

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Contact() {
     return (
         <div>
-            <h1>Contact</h1>
+        <div className='contact-area'>
+        <div className='contact-form'>
+            <form className='card' action="https://formspree.io/f/mqakojqz" method="POST">
+                <div className='card-header bg-black text-white'>
+                <h3 id='contact-header'>Contact</h3>
+                </div>
+                <div className='card-body'>
+                <div className='form-floating'>
+                <input className='form-control border-black mb-3' type='email' name='email' placeholder='Email' />
+                <label for="floatingInput">Your Email</label>
+                </div>
+                <div className='form-floating'>
+                <input className='form-control border-black' type='text' name='message' placeholder='Message'/>
+                <label for="floatingInput">Your Message</label>
+                </div>
+                <button id='contact-btn' className='btn mt-3 border border-black' type="submit">Send</button>
+                </div>
+            </form>
+        </div>
+        </div>
         </div>
     );
 }
