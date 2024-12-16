46f839b8dd7ffb708ea1231475be36196e6e84ec
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-12-16 14:40:02 -0500

    revised my info paragraph and added revised version

--- react-portfolio/src/components/info.js
+++ react-portfolio/src/components/info.js
@@ -6,15 +6,9 @@ export default function Info() {
         <div>
             <img id='profile-img' src={Profile} alt="Image of myself"></img>
             <div className='text-section'>
-            <p>Thank you for taking the time to visit my website. My name is Taylor Brandon, and for nearly two years,
-             I've been dedicated to mastering full-stack development. My journey began with an eagerness to learn and quickly adapt,
-            leading me to work on diverse projects—from personal endeavors and team activities in Columbia University's boot camp
-            to developing a professional database for a business I'm passionate about.
-             During this time, I've discovered a deep commitment to innovation, creating user-friendly applications, and a newfound
-             love for problem-solving. I've also developed a profound appreciation for technology and its constant evolution,
-            and I'm driven to connect with and learn from others.
-            This portfolio reflects my dedication and proficiency in HTML, CSS, JavaScript, web APIs, server-side APIs, Node.js,
-            OOP, Express, SQL, ORM, MVC, Regex, Mongoose, PWA, React, and MERN applications. I invite you to explore my work and welcome any feedback or inquiries.
+            <p>Welcome! Thank you for taking the time to visit my portfolio! My name is Taylor Brandon. My web development journey started over two years ago with an eagerness to learn and quickly adapt, which has led me to work on diverse projects—from personal and professional endeavors to collaborative efforts in Columbia University’s Full-Stack Development Bootcamp, where I established my programming skills.
+Since then, I have become passionate about delivering seamless, high-quality applications by honing my skills in problem-solving, accessibility, and innovation. Additionally, I have further developed my ambition to connect with and learn from others, as well as my appreciation for technology and its constant evolution.
+This portfolio demonstrates my proficiency in languages such as JavaScript and technologies such as progressive web applications, APIs, and much more! I invite you to explore my work and reach out with any feedback or inquiries!
             </p>
             </div>
         </div>
