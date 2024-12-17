2808b95f525a3743f44b9c429890bd7180b61305
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-12-17 18:35:16 -0500

    added blog website url

--- react-portfolio/src/components/portfolio.js
+++ react-portfolio/src/components/portfolio.js
@@ -7,7 +7,7 @@ export default function Portfolio() {
         {name: 'E-Commerce', description: 'Online shop that utilizes a RESTful API and also allows products to be purchased', tech: 'React, Node.js, SQL, and Express', link: 'https://github.com/Taylor-Brandon/E-Commerce-Refactor'},
         {name: 'Employee Database', description: 'Database that allows access and management to employee information', tech: 'MySQL and Object Relational Mapping', link: 'https://github.com/Taylor-Brandon/Employee-Database'},
         {name: 'Project Planner', description: 'Agenda that allows users to organize their day and save their information', tech: 'React, Third Party Api, Bulma, and Local Storage', link: 'https://github.com/Taylor-Brandon/Project-Planner-Refactor'},
-        {name: 'Blog Website', description: 'Blog application that allows user to publish their programming thoughts', tech: 'Model, View, Controler Organization, SQL, Express, Node.js, and Handlebars', link: 'https://github.com/Taylor-Brandon/Blog-Website'},
+        {name: 'Blog Website', description: 'Blog application that allows user to publish their programming thoughts', tech: 'Model, View, Controler Organization, SQL, Express, Node.js, and Handlebars', link: 'https://mysterious-woodland-92307-fc6df31f4737.herokuapp.com/'},
         {name: 'Text Editor Application', description: 'Progressive Web Application that serves are a text editor that is downloadable and works offline', tech: 'Express, Webpack, HTML Webpack Plugins, CSS Extract Plugins, Inject Manifest, Webpack Manifest, Babel-Loader', link: 'https://text-editor-app-74b27f1d557f.herokuapp.com/'},
     ]);
     console.log(projects);
